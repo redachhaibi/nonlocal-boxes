32d5a47362c7df3b81b9d0f0a4ce8fe0d571ac98
Pierre-Botteron <102599772+Pierre-Botteron@users.noreply.github.com>
2023-01-27 12:07:15 +0100

    :hammer: New file: "Gradient-Descent.ipynb"

--- non_local_boxes/utils.py
+++ non_local_boxes/utils.py
@@ -6,18 +6,18 @@ import torch
 #
 
 def W_BS09(n):  # n is the number of columns
-    W = torch.Tensor([0, 0, 1, 1,              # f_1(x, a_2) = x
-            0, 0, 1, 1,              # g_1(y, b_2) = y
-            0, 0, 0, 1,              # f_2(x, a_1) = a_1*x
-            0, 0, 0, 1,              # g_2(y, b_1) = b_1*y
-            0, 1, 1, 0, 0, 1, 1, 0,  # f_3(x, a_1, a_2) = a_1 + a_2 mod 2
-            0, 1, 1, 0, 0, 1, 1, 0   # g_3(y, b_1, b_2) = b_1 + b_2 mod 2
-            ])
-    return torch.reshape(torch.kron(torch.ones(n), W), (32, -1))
+    W = torch.tensor([0., 0., 1., 1.,              # f_1(x, a_2) = x
+            0., 0., 1., 1.,              # g_1(y, b_2) = y
+            0., 0., 0., 1.,              # f_2(x, a_1) = a_1*x
+            0., 0., 0., 1.,              # g_2(y, b_1) = b_1*y
+            0., 1., 1., 0., 0., 1., 1., 0.,  # f_3(x, a_1, a_2) = a_1 + a_2 mod 2
+            0., 1., 1., 0., 0., 1., 1., 0.   # g_3(y, b_1, b_2) = b_1 + b_2 mod 2
+            ], requires_grad=True)
+    return torch.t(W.repeat(n, 1))
 
 
 def random_wiring(n):  # n is the number of columns
-    return torch.rand((32, n))
+    return torch.rand((32, n), requires_grad=True)
 
 def random_extremal_wiring(n):  # n is the number of columns
     return torch.randint(2, (32,n))
